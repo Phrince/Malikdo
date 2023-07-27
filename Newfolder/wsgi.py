4fb377845252e9df54bf00fcf1c114f0b013834a
phrince <phrincemalikdo8525@gmail.com>
2023-07-27 23:46:10 +0100

    first commit

---
+++ Newfolder/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Newfolder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Newfolder.settings')
+
+application = get_wsgi_application()
